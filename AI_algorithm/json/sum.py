34ba5c6b6d6975b662d0fbe5c5585013fe913bd4
tcitaa <50775529+tcita@users.noreply.github.com>
2025-05-20 01:06:19 +0800

    重新训练并获取了best_genome.pkl

--- AI_algorithm/json/sum.py
+++ AI_algorithm/json/sum.py
@@ -3,7 +3,7 @@ import sys
 
 # 假设你的 JSON 数据存储在一个文件中，或者你可以直接将其作为字符串加载
 # 如果数据在文件中，请确保文件路径正确
-file_path = "data_uniq.json"
+file_path = "data_raw2.json"
 
 # 加载 JSON 数据
 with open(file_path, 'r', encoding='utf-8') as file:
