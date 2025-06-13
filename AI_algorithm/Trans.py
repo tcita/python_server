a3ca48b9cb0b7629c85bc3ab1d894da6481fc574
tcitaa <50775529+tcita@users.noreply.github.com>
2025-06-13 15:42:15 +0800

    Update Trans.py

--- AI_algorithm/Trans.py
+++ AI_algorithm/Trans.py
@@ -699,21 +699,21 @@ def Transformer_predict_batch_plus_GA(A_batch, B_batch,genomeforassist, TR_model
             moves_TR = [[int(pred_order_indices[k]), pred_moves_clipped[k]] for k in range(num_b)]
 
 
-            # #加入GA
-            # True_score_TR=strategy_TrueScore(A,B,moves_TR)
-            #
-            # move_GA=GA_Strategy(genomeforassist,A, B)
-            #
-            # True_score_GA=strategy_TrueScore(A,B,move_GA)
-            #
-            #
-            # # 比较并保存结果
-            # if True_score_GA<True_score_TR:
-            #
-            #     moves_batch[original_idx] = moves_TR
-            # # scores_batch[original_idx] = pred_score
-            # else:
-            #     moves_batch[original_idx] = move_GA
+            # todo: ⚠️这个代码块集成了GA,不需要时注释⚠️
+            True_score_TR=strategy_TrueScore(A,B,moves_TR)
+
+            move_GA=GA_Strategy(genomeforassist,A, B)
+
+            True_score_GA=strategy_TrueScore(A,B,move_GA)
+
+
+            # 比较并保存结果
+            if True_score_GA<True_score_TR:
+
+                moves_batch[original_idx] = moves_TR
+            # scores_batch[original_idx] = pred_score
+            else:
+                moves_batch[original_idx] = move_GA
 
         return moves_batch
 
@@ -790,81 +790,151 @@ def train():
                 patience=2, min_delta=0.01)
 
 
-def speed_test():
+_BLACK_HOLE_SINK = None
+
+
+def black_hole(value):
+    """
+保证解释器不优化未使用的函数  以此确保时间开销统计的准确
+    """
+    global _BLACK_HOLE_SINK
+    _BLACK_HOLE_SINK = value
+
+_BLACK_HOLE_SINK = None
+
+
+
+def black_hole(value):
+
+    """
+
+这里使用了一个小技巧,使用"black_hole"函数修改一个全局变量来保证解释器不会优化未使用的函数。
+
+
+    """
+
+    global _BLACK_HOLE_SINK
+
+    _BLACK_HOLE_SINK = value
+
+
+
+def execution_time():
+
     total_time_transformer = 0
+
     total_time_recursive = 0
-    num_iterations = 10000  # 迭代次数
+
+    num_iterations = 1000  # 迭代次数
+
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
     print(f"Starting comparison for {num_iterations} iterations...")
-    num_a_test = 6  # <--- 修改
+    # transformer预测初始化
+    num_a_test = 6
+
     num_b_test = 3
-    # 确保这些参数与训练时一致
+
+
     d_model = 256
+
     nhead = 4
+
     num_encoder_layers = 3
+
     dim_feedforward = 512
-    dropout = 0.1
+
+
 
     model1 = TransformerMovePredictor(
+
         num_a=num_a_test, num_b=num_b_test, d_model=d_model,
+
         nhead=nhead, num_encoder_layers=num_encoder_layers,
+
         dim_feedforward=dim_feedforward
+
     ).to(device)
 
+
     A_batch = []
+
     B_batch = []
 
-    model_path_1 = "./trained/transformer_move_predictor_6x3_skip.pth"
 
-    genomeforassist = load_best_genome("./trained/best_genome_skip.pkl")
 
+
+    # 辅助GA模型载入
+    genomeforassist = load_best_genome("./trained/best_genome.pkl")
+
+    model_path_1 = "./trained/transformer_move_predictor_6x3.pth"
     model1.load_state_dict(torch.load(model_path_1, map_location=device))
+
     for i in range(num_iterations):
+
         A, B = deal_cards_tool()
+
         A_batch.append(A)
+
         B_batch.append(B)
 
+
         A_copy = list(A)
+
         B_copy = list(B)
+
         start_time_r = time.perf_counter()
+
         move_r = recursive_Strategy(A_copy, B_copy)
+
+        black_hole(move_r)
+
         end_time_r = time.perf_counter()
+
         total_time_recursive += (end_time_r - start_time_r)
 
+    # 计算Transform+GA 预测 运行的时间--------------------------------------------------------------
     start_time_t = time.perf_counter()
+
     move_t = Transformer_predict_batch_plus_GA(A_batch, B_batch,genomeforassist, model1, num_a=num_a_test, num_b=num_b_test)
 
-    # 模拟将得到的列表中的元素取出操作
-    for item in move_t:
-        print(" ")
 
+    # 因为得到的返回值move是批次的(列表),这里模拟将得到的列表中的元素取出操作消耗的时间
+
+    for item in move_t:
+        print(item)
 
 
     end_time_t = time.perf_counter()
+
     total_time_transformer += (end_time_t - start_time_t)
+    # --------------------------------------------------------------------------------------------
 
-    #
-    # # 打开文件并逐行写入
-    # with open("output.txt", "w", encoding="utf-8") as file:
-    #     for row in move_t:
-    #         file.write(" ".join(map(str, row)) + "\n")  # 每行用空格分隔
 
-    # --- 报告结果 ---
+
     print("\n--- Comparison Complete ---")
+
     print(f"Total time for Transformer:      {total_time_transformer:.6f} seconds")
+
 
     print(f"Total time for recursive_Strategy: {total_time_recursive:.6f} seconds")
 
+
     if num_iterations > 0:
+
         avg_time_transformer = total_time_transformer / num_iterations
+
         print(f"\nAverage time per call (Transformer):      {avg_time_transformer:.8f} seconds")
 
+
         avg_time_recursive = total_time_recursive / num_iterations
+
         print(f"Average time per call (recursive_Strategy): {avg_time_recursive:.8f} seconds")
+
 
 
 if __name__ == "__main__":
     # train()
 
-    speed_test()
+    execution_time()
